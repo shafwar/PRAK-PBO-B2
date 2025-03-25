2fa3572364b8b73a0613bcc966a4b91f2e3ed5d9
shafwar <naufalshafi15@gmail.com>
2025-03-25 17:19:24 +0700

    Add files via upload

---
+++ PRAK PBO 2/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    // Constructor sesuai dengan yang digunakan di Main.java
+    public Main(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    // Getter methods (optional, if needed)
+    public String getKode() {
+        return kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    // Tambahkan metode lain jika diperlukan
+}
