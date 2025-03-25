30285f7286a551c2de2d195d7acf0c7709da8c9f
shafwar <naufalshafi15@gmail.com>
2025-03-25 16:34:53 +0700

    Add files via upload

---
+++ PBO Responsi/Universitas/Fakultas.java
@@ -0,0 +1,23 @@
+public class Fakultas {
+    private String nama;
+    private double tarifUKT;
+    private double gajiPokok;
+
+    public Fakultas(String nama, double tarifUKT, double gajiPokok) {
+        this.nama = nama;
+        this.tarifUKT = tarifUKT;
+        this.gajiPokok = gajiPokok;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getTarifUKT() {
+        return tarifUKT;
+    }
+
+    public double getGajiPokok() {
+        return gajiPokok;
+    }
+}
